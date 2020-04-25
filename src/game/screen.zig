58f1305dc7bcecf0fa303cc228812ec21a3af90a
Louis Pearson <louispearson@librem.one>
2020-04-25 03:24:41 -0600

    Add kb movement in again

--- src/game/screen.zig
+++ src/game/screen.zig
@@ -296,21 +296,21 @@ pub const Screen = struct {
         self.look_angle.v -= ANALOG_SPEED * @floatCast(f32, delta) * look_v_amt;
         self.look_angle.v = std.math.clamp(self.look_angle.v, @as(f32, -std.math.pi * 2.0 / 5.0), std.math.pi * 2.0 / 5.0);
         var forward_amt: f32 = 0.0;
+        forward_amt = -@intToFloat(f32, self.ctrlr_axis.left_v) / @intToFloat(f32, std.math.maxInt(i16));
         if (self.iskeydown.forward) {
             forward_amt += 1;
         }
         if (self.iskeydown.backward) {
             forward_amt -= 1;
         }
-        forward_amt = -@intToFloat(f32, self.ctrlr_axis.left_v) / @intToFloat(f32, std.math.maxInt(i16));
         var side_amt: f32 = 0;
+        side_amt = @intToFloat(f32, self.ctrlr_axis.left_h) / @intToFloat(f32, std.math.maxInt(i16));
         if (self.iskeydown.right) {
             side_amt += 1;
         }
         if (self.iskeydown.left) {
             side_amt -= 1;
         }
-        side_amt = @intToFloat(f32, self.ctrlr_axis.left_h) / @intToFloat(f32, std.math.maxInt(i16));
         var vert_amt: f32 = 0;
         if (self.iskeydown.up) {
             vert_amt += 1;
